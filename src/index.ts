d90004abb351c280d9fe8b96bbfba84c481c221f
Rohan Sunwar <rohansunwar5@gmail.com>
2025-11-20 12:47:52 +0530

    updated index.js

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import os from 'os';
-import cluster from 'cluster';
 import app from './app';
 import logger from './utils/logger';
 import connectDB from './db';
@@ -12,65 +9,9 @@ import redisClient from './services/cache';
   logger.info('DB connected');
   await redisClient.connect();
 
-  const numCPUs = process.env.NODE_ENV === 'production' ? os.cpus().length : 1;
-  if (cluster.isMaster) {
-    logger.info(`Master ${process.pid} is running`);
+ const port = parseInt(process.env.PORT || "4010", 10);
 
-    // Fork workers...
-    for (let i = 0; i < numCPUs; i++) {
-      cluster.fork();
-    }
-
-    cluster.on('exit', (worker) => {
-      logger.info(`worker ${worker.process.pid} died`);
-      const newWorker = cluster.fork();
-      logger.info(`fork-worker: New worker started with PID - ${newWorker.process.pid}`);
-    });
-  } else {
-    const normalizePort = (val: any) => {
-      const port = parseInt(val, 10);
-
-      if (Number.isNaN(port)) {
-        return val;
-      }
-
-      if (port >= 0) {
-        return port;
-      }
-
-      return false;
-    };
-
-    const port = normalizePort(process.env.PORT || '4010');
-
-    const onError = (error: any) => {
-      if (error.syscall !== 'listen') {
-        throw error;
-      }
-
-      const bind = typeof port === 'string' ? `pipe ${port}` : `port ${port}`;
-      switch (error.code) {
-        case 'EACCES':
-          logger.error(`${bind} requires elevated privileges`);
-          process.exit(1);
-          break;
-        case 'EADDRINUSE':
-          logger.error(`${bind} is already in use`);
-          process.exit(1);
-          break;
-        default:
-          throw error;
-      }
-    };
-
-    app.on('error', onError);
-    const onListening = () => {
-      const bind = typeof port === 'string' ? `pipe ${port}` : `port ${port}`;
-      logger.info(`Listening on ${bind}`);
-      logger.info('Connected!');
-    };
-
-    app.listen(port, onListening);
-    logger.info(`Worker ${process.pid} started`);
-  }
+app.listen(port, "0.0.0.0", () => {
+  logger.info(`Server running on port ${port}`);
+});
 })();
